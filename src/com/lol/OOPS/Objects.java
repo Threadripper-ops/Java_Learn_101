ac213a6bf2e282ca4a58cc0d1775c56689d64c22
Threadripper-ops <123474729+Threadripper-ops@users.noreply.github.com>
2023-06-15 00:47:10 +0530

    Create README.md

--- src/com/lol/OOPS/Objects.java
+++ src/com/lol/OOPS/Objects.java
@@ -9,9 +9,31 @@ public class Objects {
       //  s1.marks = 90.67f;
       //  s1.name = "Harsh";
        // s1.rno = 12;
+        Student s2 = new Student(13, "shivam", 90.67f);
+        System.out.println(s2.marks);
+        System.out.println(s2.name);
+
+
+
+
+        s1.greeting();
+
+      //  s1.changeName("Sneakerhead");
         s1.greeting();
 
         System.out.println(s1.rno);
+         Student random = new Student(s1);
+        System.out.println(random.name);
+
+        Student random2 = new Student();
+        System.out.println(random2.name);
+
+
+
+
+
+
+
 
     }
 }
@@ -35,6 +57,10 @@ class Student {
         System.out.println("Hello i am "+ name);
     }
 
+    void changeName(String newName){
+        name = newName;
+    }
+
     // to pre initialize the objects
     // we use constructors
     // constructors are special methods
@@ -43,14 +69,48 @@ class Student {
 
     /// we need a way to add valuees of above proPERTIRS
     // object by object
+////
+
     Student(){
+
+        //this how you call a constructor from
+        // another constructor
+        this(13, "default person", 100.0f); // internally
+        // this is like new Student();
+
+    }
+    Student( Student other){
+        this.rno = other.rno;
+        this.name= other.name;
+        this.marks = other.marks;
+
+    }
+    /*Student(){
         this.rno = 123;
         // this keyword is used to
         // refer to the current object
         this.name = "Harsh";
         this.marks = 90.67f;
-    }
+        }
+        */
 
+
+    /// eg Student s2 = new Student(13, "arpit", 90.67f);
+
+    /// here this will be replaced by s2
+
+    // this called initializing the object
+
+    Student(int rno, String naam, Float marks){//these are called parameters
+        this.rno = 123;
+                                   // this keyword is used to
+                                // refer to the current object
+        this.name = naam;           // you con skip using this if the
+                              // name of the parameter is different from the name of the property
+        this.marks = marks; // But it is a good practice
+                            // to use this
+
+    }
 
 
 
