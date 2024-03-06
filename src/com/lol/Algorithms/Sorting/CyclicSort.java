c9cf74b3251f5f7f5d8cb0c232d26c82982fb28c
Threadripper-ops <123474729+Threadripper-ops@users.noreply.github.com>
2024-03-06 22:41:37 +0530

    Progress

---
+++ src/com/lol/Algorithms/Sorting/CyclicSort.java
@@ -0,0 +1,34 @@
+package com.lol.Algorithms.Sorting;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr= {3,5,2,1,4};
+        Cyclic(arr);
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+
+    static void Cyclic(int[] arr){
+        int i=0;
+
+        while(i<arr.length){
+            int corr = arr[i]-1;
+            if(arr[i]!=arr[corr]){  //
+                swap(arr,i,corr);// we are taking the first index element and swapping it with its correct index element
+                
+            }else i++
+            ;
+        }
+
+    }
+
+    static void swap(int[] arr,int a,int b){
+        int temp = arr[a];
+        arr[a]=arr[b];
+        arr[b]=temp;
+    }
+
+}
--- src/com/lol/Algorithms/Sorting/InssertionSort.java
+++ src/com/lol/Algorithms/Sorting/InssertionSort.java
@@ -1,4 +1,35 @@
 package com.lol.Algorithms.Sorting;
 
+import java.util.Arrays;
+
 public class InssertionSort {
+    public static void main(String[] args) {
+        int[] arr= {2,4,1,4,2,3,-33,-43};
+        Sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+
+    static void Sort(int[] arr){
+        for (int i = 0; i <= arr.length-2; i++) {
+            for (int j = i+1; j> 0 ; j--) {
+
+                if(arr[j]<arr[j-1]){
+                    swap(arr,j,j-1);
+                }else{
+                    break;
+                }
+
+            }
+
+        }
+    }
+
+    private static void swap(int[] arr,int a, int b) {
+        int temp =arr[a];
+        arr[a]=arr[b];
+        arr[b]=temp;
+
+    }
 }
